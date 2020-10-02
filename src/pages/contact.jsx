98abfa15835e30090ed3515556dd96cdbe293f6f
Maddie <maddiemcampbell@gmail.com>
2020-10-01 18:05:01 -0700

    updates logo and adds embedded contact form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,12 +3,7 @@ import Layout from "components/Layout";
 
 const contact = () => (
     <Layout>
-    <h1>
-        Contact
-    </h1>
-    <p>
-        Contact Form will go here
-    </p>
+   <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfeu7hHLrh9Nd3wlavIZLdmj28rckNCRnvX-893s-PYOwCoEw/viewform?embedded=true" width="640" height="943" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 </Layout>
 );
 
